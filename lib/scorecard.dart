848f03c6d26e73c22a45de3fdbed6556164cfa6a
Mikey Stewart <mikey.stewart@trademe.co.nz>
2019-09-15 16:11:30 +1200

    add ability to decrement score

--- lib/scorecard.dart
+++ lib/scorecard.dart
@@ -2,78 +2,93 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class Scorecard extends StatefulWidget {
-	Scorecard({Key key, this.teamName, this.nameColour, this.cardColour}) : super(key: key);
+  Scorecard({Key key, this.teamName, this.nameColour, this.cardColour})
+      : super(key: key);
 
-	final String teamName;
-	final Color nameColour;
-	final Color cardColour;
+  final String teamName;
+  final Color nameColour;
+  final Color cardColour;
 
-	@override
-	_ScorecardState createState() => _ScorecardState();
+  @override
+  _ScorecardState createState() => _ScorecardState();
 }
 
 class _ScorecardState extends State<Scorecard> {
-	int _counter = 0;
+  int _counter = 0;
 
-	void _incrementCounter() {
-		setState(() {
-			_counter++;
-		});
-	}
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
 
-	void _resetCounter() {
-		setState(() {
-			_counter = 0;
-		});
-	}
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
 
-	@override
-	Widget build(BuildContext context) {
-		return Expanded(
-			child: InkWell(
-				onTap: () {
-					_incrementCounter();
-				},
-				onLongPress: () {
-					_resetCounter();
-				},
-				child: Card(
-					clipBehavior: Clip.antiAlias,
-					margin: EdgeInsets.all(32.0),
-					color: widget.cardColour,
-					shape: RoundedRectangleBorder(
-						borderRadius: BorderRadius.circular(15)
-					),
-					child: Row(
-						children: <Widget>[
-							Expanded(
-								child: Column(
-									children: <Widget>[
-										Container(
-											color: widget.nameColour,
-											height: 50,
-											child: SizedBox.expand(
-												child: Center(
-													child: Text(
-														widget.teamName
-													)
-												),
-											),
-										),
-										Expanded(
-											child: Center(
-												child: Text(
-													_counter.toString()
-												),
-											),
-										)
-									],
-								)
-							)
-						],
-					),
-				),
-			)
-		);
-	}
+  void _resetCounter() {
+    setState(() {
+      _counter = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: InkWell(
+      onLongPress: () {
+        _resetCounter();
+      },
+      child: Card(
+        clipBehavior: Clip.antiAlias,
+        margin: EdgeInsets.all(32.0),
+        color: widget.cardColour,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+                child: Column(
+              children: <Widget>[
+                Container(
+                  color: widget.nameColour,
+                  height: 50,
+                  child: SizedBox.expand(
+                    child: Center(child: Text(widget.teamName)),
+                  ),
+                ),
+                Expanded(
+                    child: Stack(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: InkWell(
+                            onTap: () {
+                              _decrementCounter();
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          child: InkWell(
+                            onTap: () {
+                              _incrementCounter();
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                    Center(
+                      child: Text(_counter.toString()),
+                    ),
+                  ],
+                ))
+              ],
+            ))
+          ],
+        ),
+      ),
+    ));
+  }
 }
